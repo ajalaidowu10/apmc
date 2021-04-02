d7489d9a8be4948186e0bc40c16f82a3ea48d032
Ajala Idowu <ajalaidowu10@gmail.com>
2021-04-02 13:15:52 +0100

    Done

--- app/Company.php
+++ app/Company.php
@@ -26,7 +26,7 @@ class Company extends Model
 
    public function getInvfooterPathAttribute()
    {
-       return asset(\Storage::url($this->invheader ? 'images/company/'.$this->invheader : 'images/company/default.png'));
+       return asset(\Storage::url($this->invfooter ? 'images/company/'.$this->invfooter : 'images/company/default.png'));
    }
 
    public function getRecheaderPathAttribute()
@@ -36,7 +36,7 @@ class Company extends Model
 
    public function getRecfooterPathAttribute()
    {
-       return asset(\Storage::url($this->recheader ? 'images/company/'.$this->recheader : 'images/company/default.png'));
+       return asset(\Storage::url($this->recfooter ? 'images/company/'.$this->recfooter : 'images/company/default.png'));
    }
 
 }
