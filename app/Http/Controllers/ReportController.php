4114c15b46a8e68e37b3691f4fb1a16558b7a8c5
Ajala Idowu <ajalaidowu10@gmail.com>
2021-04-09 08:05:56 +0100

    WIP

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -280,28 +280,48 @@ class ReportController extends Controller
         return 0;
       }
       
-
-
       return number_format((float)$result, 2, '.', '');
-
     }
+
+    public function open_bal(string $date_to):float
+    {
+      $result = 0;
+
+      $open_bal = $this->getLedgerWhere($date_to, 0, 0, 0,1);
+
+      foreach ($open_bal as $key) 
+      {
+        $result = $result + $key->result;
+      }
+
+      if ($result < 0) 
+      {
+        return 0;
+      }
+      
+      return number_format((float)$result, 2, '.', '');
+    }
+
+
 
 
 
     public function getBalsheet(string $date_to)
     {
       $finyear_from = Auth::guard('admin')->user()->finyear->from_date;
-      $asset_liability     = $this->getLedgerWhere($date_to, 0, 2, 0,0);
+      $asset_liability    = $this->getLedgerWhere($date_to, 0, 2, 0,0);
       $stock_value        = $this->stock_value($date_to);
-      $prev_profit_loss    = $this->getProfitLoss($finyear_from);
-      $profit_loss         = $this->getProfitLoss($date_to) - $prev_profit_loss;
+      $prev_profit_loss   = $this->getProfitLoss($finyear_from);
+      $profit_loss        = $this->getProfitLoss($date_to) - $prev_profit_loss;
+      $openbal_diff       = $this->open_bal($date_to);
       
 
       return [
-                'asset_liability' => $asset_liability, 
-                'prev_profit_loss' => $prev_profit_loss, 
-                'profit_loss' => $profit_loss,
-                'stock_value' => $stock_value, 
+                'asset_liability'   => $asset_liability, 
+                'prev_profit_loss'  => $prev_profit_loss, 
+                'profit_loss'       => $profit_loss,
+                'stock_value'       => $stock_value, 
+                'openbal_diff'      => $openbal_diff, 
 
               ];
 
