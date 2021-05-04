961042536d249e7c500c7ae50fea2c1c7c3722d6
Ajala Idowu <ajalaidowu10@gmail.com>
2021-05-04 20:57:06 +0100

    - Add account popup window to sales

--- resources/js/components/admin/sales/Add.vue
+++ resources/js/components/admin/sales/Add.vue
@@ -69,6 +69,8 @@
                   dense
                   @change="setAcct($event)"
                   outlined
+                  append-outer-icon="mdi-plus"
+                  @click:append-outer="addNewAccount"
                 >
                 </v-combobox>
               </v-col>
@@ -668,6 +670,32 @@
       
     },
     methods: {
+          addNewAccount(){
+            let routeData = this.$router.resolve({path: 'account',  params:{
+                                                                                        dateTo:'2021-05-03'
+                                                                                      }});
+
+            this.popupwindow(routeData.href, "Add Account", 500, 500);
+          },
+          popupwindow(url, title, w, h) {
+            var left = (screen.width/2)-(w/2);
+            var top = (screen.height/2)-(h/2);
+            var win = window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+            var timer = setInterval(() => { 
+                if(win.closed) {
+                    clearInterval(timer);
+                    axios.get(`account/get/0/2`)
+                          .then(resp=>{
+                            this.acct = transformKeys.camelCase(resp.data.data);
+                            console.log('hello');
+                          })
+                          .catch(err => Exception.handle(err, 'admin'));
+                }
+            }, 1000);
+
+            return win;
+          },
+
           setItem(data){
             this.form.itemId = data.itemId;
             this.form.item = data.item;
